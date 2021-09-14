4f50fd1c7db77cdd79ba9de3621ebb167ade629e
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-09-14 17:13:52 +0200

    Beta 3

--- UEFI version/kernel/kernel.c
+++ UEFI version/kernel/kernel.c
@@ -1,4 +1,23 @@
-int _start() 
+
+typedef unsigned long long size_t;
+
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} Framebuffer;
+
+void _start(Framebuffer* framebuffer) 
 {
-    return 123;
+    unsigned int y = 50;
+	unsigned int BBP = 4;
+
+	for(unsigned int x = 0; x < framebuffer->Width / 2 * BBP; x+=BBP) {
+		*(unsigned int*)(x + (y * framebuffer->PixelsPerScanLine * BBP) + framebuffer->BaseAddress) = 0xff00ffff;
+	}
+
+
+    return ;
 }
