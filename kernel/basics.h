7735b028c5a2af9846a8810fdf2b194f33b6cca2
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-01-04 18:55:52 +0100

    Deleted some files

--- kernel/basics.h
+++
@@ -1,3 +0,0 @@
-
-//edited implementention of itoa https://www.techcrashcourse.com/2016/02/c-program-to-implement-your-own-itoa-function.html
-void itoa(unsigned long long int num,char* str,int base);
