64e5ad1c8a86c97c1ab2b95e5faccb0ff54f874e
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2022-01-30 19:51:38 +0100

    Add files via upload

---
+++ ringOS-Beta17/src/shell/types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char int8;
+typedef unsigned char uint8;
+
+typedef signed short int16;
+typedef unsigned short uint16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+
+typedef signed long long int64;
+typedef unsigned long long uint64;
+
+typedef char* string; 
+
+#endif
