44d0b3d87cef985ce18a49464f1a6eb7ba6e0543
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-01-31 20:02:16 +0100

    Text file

--- POSIX/main.c
+++ POSIX/main.c
@@ -2,8 +2,38 @@
 
 int main (int argc, char** argv)
 {
-    printf("Hello World!!\n");
-    
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_BLUE);
+    printf("Welcome to ringOS POSIX!! \n");
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
+
+    (void)argc;
+    (void)argv;
+    FILE *f;
+    char *buff;
+    long int size;
+
+    if((f = fopen("file.txt", "r"))) 
+    {
+        fseek(f, 0, SEEK_END);
+        size = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        printf("File size: %d bytes\n\n", size);
+        buff = malloc(size + 1);
+        if(!buff) 
+        {
+            fprintf(stderr, "unable to allocate memory\n");
+            return 1;
+        }
+        fread(buff, size, 1, f);
+        buff[size] = 0;
+        fclose(f);
+        printf("File contents:\n%s\n", buff);
+        free(buff);
+    } 
+    else
+        fprintf(stderr, "Unable to open file\n");
+
+
     while(1);
     return 0;
 }
