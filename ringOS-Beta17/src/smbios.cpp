d3b7e5af9c9e8d4f175c1d0b8c664ab4769d6c9c
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-02-12 20:42:18 +0100

    Update smbios.cpp

--- ringOS-Beta17/src/smbios.cpp
+++ ringOS-Beta17/src/smbios.cpp
@@ -6,12 +6,5 @@ SMBiosHeader* hdr;
 
 void SMBiosParse(SMBiosHeader* hdr)
 {
-    SMBiosEntry* entry = (SMBiosEntry*)((uint64_t)hdr->TableAddress);
-    printf("SMBios entries: %u\n",hdr->TableLength);
-    for(int i = 0; i < hdr->TableLength; i++)
-    {
-        printf("%u ",entry->Type);
-        entry += entry->Lenght+sizeof(SMBiosEntry);
-    }
-    printf("\n");
+    
 }
