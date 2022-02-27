efa5806743a0eeb9e8ce0b0a686bb59645b6e085
Andrej <andrej.bartulinvr@gmail.com>
2022-02-27 13:49:05 +0100

    Shell script bug fix

--- ringOS-beta.sh
+++ ringOS-beta.sh
@@ -9,9 +9,6 @@ sudo apt-get install lld && echo lld installed successfully || echo FAILED to in
 sudo apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
 sudo apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
 
-git clone -b main https://github.com/ringwormGO-organization/ringOS
-cd ringOS
-
 git clone https://github.com/VanGutan/gnu-efi
 mv bootloader gnu-efi/bootloader
 
