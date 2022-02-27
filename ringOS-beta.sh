74ac6a82053f1c254af7f827a828ff9867b2317b
Andrej <andrej.bartulinvr@gmail.com>
2022-02-27 13:56:37 +0100

    OVMFbin

--- ringOS-beta.sh
+++ ringOS-beta.sh
@@ -12,13 +12,11 @@ sudo apt-get install mkisofs && echo mkisofs installed successfully || echo FAIL
 git clone https://github.com/VanGutan/gnu-efi
 mv bootloader gnu-efi/bootloader
 
-git clone https://github.com/VanGutan/ringOS-Beta14-OVMFTemplate
-mv ringOS-Beta14-OVMFTemplate OVMFbin
-
 cd gnu-efi && echo In gnu-efi directory || echo FAILED to enter gnu-efi directory
 make bootloader && echo Bootloader successfully created || echo FAILED to create bootloader
 cd ../kernel && echo In kernel directory || echo FAILED to enter kernel directory
 make kernel && echo Kernel successfully created || echo FAILED to create kernel
 make buildimg && echo Buildimg successfully created || echo FAILED to create buildimg
+chmod +x ./ISO.sh
 ./ISO.sh
 make run
