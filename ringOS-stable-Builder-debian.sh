b5593e057db599e61b664d9f4fa39de5eed8144b
Someone <hahahahacker2009@outlook.com>
2022-04-20 07:30:16 +0700

    create script to download and compile ringOS beta for Red Hat

--- ringOS-stable-Builder-debian.sh
+++ ringOS-stable-Builder-debian.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-clear
+echo "ringOS stable builder for Debian(-based) Linux"
+echo "This script will ask you for password using sudo."
 
-sudo apt update && echo Update successful || echo FAILED to update
-sudo apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential tools
+sudo apt-get update && echo Repository update successful || echo FAILED to update the repository
+sudo apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential mtools
 sudo apt-get install nasm && echo NASM installed successfully || echo FAILED to install nasm
 sudo apt-get install qemu-system-x86 && echo Qemu installed successfully || echo FAILED to install Qemu
 sudo apt-get install git && echo git installed successfully || echo FAILED to install git
@@ -11,9 +12,15 @@ sudo apt-get install lld && echo lld installed successfully || echo FAILED to in
 sudo apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
 sudo apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
 
-git clone https://github.com/VanGutan/gnu-efi
-mv bootloader gnu-efi/bootloader
+mkdir "ringOS-stable" && echo ringOS-stable directory successfully created || echo FAILED to create ringOS directory
 
+cd ringOS-stable && echo Successfully enter ringOS-stable directory || echo FAILED to enter ringOS-stable directory
+
+git clone -b ringOS-stable2 https://github.com/ringwormGO-organization/ringOS && echo Cloning successful || echo FAILED to clone
+
+cd ringOS && echo In ringOS directory || echo FAILED to enter ringOS directory
+
+cd ringOS && echo In ringOS directory || echo FAILED to enter ringOS directory
 cd gnu-efi && echo In gnu-efi directory || echo FAILED to enter gnu-efi directory
 make bootloader && echo Bootloader successfully created || echo FAILED to create bootloader
 cd ../kernel && echo In kernel directory || echo FAILED to enter kernel directory
@@ -21,8 +28,4 @@ make kernel && echo Kernel successfully created || echo FAILED to create kernel
 make buildimg && echo Buildimg successfully created || echo FAILED to create buildimg
 chmod +x ./ISO.sh
 ./ISO.sh
-make run
-
-cd ../
-chmod +x ./clean.sh
-./clean.sh
+make run && echo Successfully run ringOS stable release || echo FAILED run ringOS stable release
