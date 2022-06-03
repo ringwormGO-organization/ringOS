06538aa82cd3aea3a6cc7e15a8acd00d17f84d5a
Someone <hahahahacker2009@outlook.com>
2022-06-03 16:48:29 +0700

    Rewrite script to fix character encoding error, remove sudo for easier build in container

--- install-scripts/ringOS-beta-debian.sh
+++ install-scripts/ringOS-beta-debian.sh
@@ -3,14 +3,14 @@
 echo "ringOS beta builder for Debian(-based) Linux"
 echo "This script will ask you for password using sudo."
 
-sudo apt-get update && echo Repository update successful || echo FAILED to update the repository
-sudo apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential and/or mtools
-sudo apt-get install nasm && echo NASM installed successfully || echo FAILED to install nasm
-sudo apt-get install qemu-system-x86 && echo Qemu installed successfully || echo FAILED to install Qemu
-sudo apt-get install git && echo git installed successfully || echo FAILED to install git
-sudo apt-get install lld && echo lld installed successfully || echo FAILED to install lld
-sudo apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
-sudo apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
+apt-get update && echo Repository update successful || echo FAILED to update the repository
+apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential and/or mtools
+apt-get install nasm && echo NASM installed successfully || echo FAILED to install nasm
+apt-get install qemu-system-x86 && echo Qemu installed successfully || echo FAILED to install Qemu
+apt-get install git && echo git installed successfully || echo FAILED to install git
+apt-get install lld && echo lld installed successfully || echo FAILED to install lld
+apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
+apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
 
 git clone https://github.com/ringwormGO-organization/ringOS
 
@@ -18,3 +18,4 @@ cd ringOS
 
 chmod +x ./compile.sh
 ./compile.sh
+
