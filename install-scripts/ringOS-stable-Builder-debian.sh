6e7df9824ee06feaa250dd356af452368fac9b5c
Someone <hahahahacker2009@outlook.com>
2022-06-03 16:51:15 +0700

    Rewrite script to fix character encoding error, remove sudo for easier build in container

--- install-scripts/ringOS-stable-Builder-debian.sh
+++ install-scripts/ringOS-stable-Builder-debian.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "ringOS stable builder for Debian(-based) Linux"
-echo "This script will ask you for password using sudo."
+echo "This script will ask you for password using ."
 
-sudo apt-get update && echo Repository update successful || echo FAILED to update the repository
-sudo apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential mtools
-sudo apt-get install nasm && echo NASM installed successfully || echo FAILED to install nasm
-sudo apt-get install qemu-system-x86 && echo Qemu installed successfully || echo FAILED to install Qemu
-sudo apt-get install git && echo git installed successfully || echo FAILED to install git
-sudo apt-get install lld && echo lld installed successfully || echo FAILED to install lld
-sudo apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
-sudo apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
+apt-get update && echo Repository update successful || echo FAILED to update the repository
+apt-get install build-essential mtools && echo build-essential mtools installed successfully || echo FAILED to install build-essential mtools
+apt-get install nasm && echo NASM installed successfully || echo FAILED to install nasm
+apt-get install qemu-system-x86 && echo Qemu installed successfully || echo FAILED to install Qemu
+apt-get install git && echo git installed successfully || echo FAILED to install git
+apt-get install lld && echo lld installed successfully || echo FAILED to install lld
+apt-get install xorriso && echo xorriso installed successfully || echo FAILED to install xorriso
+apt-get install mkisofs && echo mkisofs installed successfully || echo FAILED to install mkisofs
 
 mkdir "ringOS-stable" && echo ringOS-stable directory successfully created || echo FAILED to create ringOS directory
 
@@ -29,3 +29,4 @@ make buildimg && echo Buildimg successfully created || echo FAILED to create bui
 chmod +x ./ISO.sh
 ./ISO.sh
 make run && echo Successfully run ringOS stable release || echo FAILED run ringOS stable release
+
